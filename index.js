271c6cafa48548b36d390081e8c067416fa88af6
Ashwin Sheoran <88393756+AshwinSheoran02@users.noreply.github.com>
2022-08-20 20:46:16 +0530

    4th

--- index.js
+++ index.js
@@ -10,22 +10,39 @@ let newsaccordion = document.getElementById('newsaccordion');
 const xhr = new XMLHttpRequest();
 //xhr.open('POST' ,'https://newsapi.org/v2/top-headlines/sources?apiKey=75098fa5661e4e1985f6826272b071dd' , true );
 xhr.open('GET', `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apiKey}`, true);
-xhr.getAllResponseHeaders('Content-type' , 'application/json');
+//xhr.getAllResponseHeaders('Content-type' , 'application/json');
 
-let news = `
-<div class="card">
-<div class="card-header" id="headingOne">
-  <h5 class="mb-0">
-    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-      Collapsible Group Item #1
-    </button>
-  </h5>
-</div>
+xhr.onload = function () {
+    if (this.status === 200) {
+        let json = JSON.parse(this.responseText);
+        let articles = json.articles;
+        console.log(articles);
+        let newsHtml = "";
+        articles.forEach(function(element, index) {
+            // console.log(element, index)
+            let news = `<div class="card">
+                            <div class="card-header" id="heading${index}">
+                                <h2 class="mb-0">
+                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
+                                    aria-expanded="false" aria-controls="collapse${index}">
+                                   <h5> <b>Breaking News ${index+1}:</b> ${element["title"] } 
+                                   🔽
+                                </button>
+                                <h5>
+                                </h2>
+                            </div>
 
-<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-  <div class="card-body">
-    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-  </div>
-</div>
-</div>
-`;
+                            <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}" data-parent="#newsaccordion">
+                                <div class="card-body"> ${element["content"]}. <a href="${element['url']}" target="_blank" >Read more about this here</a>  </div>
+                            </div>
+                        </div>`;
+            newsHtml += news;
+        });
+        newsaccordion.innerHTML = newsHtml;
+    }
+    else {
+        console.log("Some error occured")
+    }
+}
+
+xhr.send()
